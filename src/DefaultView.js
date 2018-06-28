5f28bea84818954ff7615fd5239b07998eed1558
1n3ffbl3 <mizgier.m@gmail.com>
2018-06-28 21:23:27 +0200

    Show-hide mechanisme

--- src/DefaultView.js
+++ src/DefaultView.js
@@ -2,19 +2,30 @@ import React, {Component} from "react";
 import SubView from "./SubView";
 
 class DefaultView extends Component{
+    constructor(){
+        super();
+        this.state = {
+            showSubView: false
+        };
+    }
     render(){
         const subView2 = (
             <SubView name= "Second SubView"
-                     content= "Second subview content"
+                    content= "Second subview content"
                      />
         );
         return(
             <div>
                 <div>Default View</div>
-                    <SubView name="First SubView" 
-                             content="First subview content" 
-                             nestedView={subView2}
-                        />
+                <button onClick={()=> this.setState({showSubView: !this.state.showSubView})}>Trigger SubView</button>
+                {
+                    this.state.showSubView ?   <SubView name="First SubView" 
+                        content="First subview content" 
+                        nestedView={subView2}
+                         /> : ""
+                }
+            
+               
             </div>
         )
     }
